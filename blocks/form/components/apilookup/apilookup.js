83a7beec5095c425060c3d5d6334fd356c0722dc
ajaniadobe <ajani@adobe.com>
2025-08-05 05:08:54 -0400

    add api lookup form field

---
+++ blocks/form/components/apilookup/apilookup.js
@@ -0,0 +1,60 @@
+const textIntersectionClass = 'apilookup__text-intersect';
+const textDecorationClass = 'apilookup__text-decoration';
+
+class ApiLookup {
+  constructor(fieldDiv, fieldJson) {
+    this.fieldDiv = fieldDiv;
+    this.fieldJson = fieldJson;
+    this.formModel = null;
+    this.decorate();
+  }
+
+  setFormModel(model) {
+    this.formModel = model;
+  }
+
+  getfieldDiv() {
+    return this.fieldDiv;
+  }
+
+  decorate() {
+    const textWrapper = this.fieldDiv.querySelector('.plain-text-wrapper');
+    const helpText = this.fieldDiv.querySelector('.field-description');
+    if (helpText) {
+      this.fieldDiv.append(helpText);
+    }
+    if (!textWrapper) { // rendition does not have a plain-text-wrapper => link rendition of apilookup
+      // eslint-disable-next-line no-console
+      console.debug('No plain-text found in apilookup field. Assuming Link based rendition and Skipping decoration.');
+      this.fieldDiv.classList.add('link');
+      return;
+    }
+    textWrapper.classList.add(textDecorationClass);
+    const intersection = document.createElement('div');
+    intersection.classList.add(textIntersectionClass);
+    textWrapper.appendChild(intersection);
+    this.handleScroll();
+  }
+
+  handleScroll() {
+    const intersection = this.fieldDiv.querySelector(`.${textIntersectionClass}`);
+    if (intersection) {
+      const io = new IntersectionObserver(([{ isIntersecting }]) => {
+        if (isIntersecting) {
+          /*
+          * TODO: Enable the checkboxes that are disabled by default via the model.
+          *  Currently they are enabled by default
+          * */
+          io.unobserve(intersection);
+        }
+      }, {
+        threshold: [1],
+      });
+      io.observe(intersection);
+    }
+  }
+}
+export default async function decorate(apilookupDiv, fieldJson) {
+  const apilookup = new TermsAndConditions(apilookupDiv, fieldJson);
+  return apilookup.getfieldDiv();
+}
